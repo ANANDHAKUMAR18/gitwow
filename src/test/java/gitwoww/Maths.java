9fe05fddca2225afc24837028e1522b2c5f3d99f
AK <anandhsandy.g@gmail.com>
2023-10-31 03:50:12 +0530

    First Commit

---
+++ src/test/java/gitwoww/Maths.java
@@ -0,0 +1,12 @@
+package gitwoww;
+
+public class Maths {
+	
+	public void Info1(){
+		System.out.println("Hello World");
+		}
+	public static void main(String[] args){
+        Maths obj=new Maths();
+	obj.Info1();
+}
+}
