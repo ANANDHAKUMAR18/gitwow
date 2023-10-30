77bac726109e10a3f263883999b81d30837f97a2
Sandhiya0205 <146726602+Sandhiya0205@users.noreply.github.com>
2023-10-31 03:56:06 +0530

    Update Maths.java

--- src/test/java/gitwoww/Maths.java
+++ src/test/java/gitwoww/Maths.java
@@ -5,8 +5,12 @@ public class Maths {
 	public void Info1(){
 		System.out.println("Hello World");
 		}
+	public void Info3(){
+		System.out.println("Hello World");
+		}
 	public static void main(String[] args){
         Maths obj=new Maths();
 	obj.Info1();
+	obj.Info3();
 }
 }
